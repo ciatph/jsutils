11bf4d7a5a153c7f8d16110422898255a2a39edf
ciatph <ciat.ph@gmail.com>
2018-11-13 10:36:39 +0700

    Added JS Date methods.

--- scripts/date.js
+++ scripts/date.js
@@ -19,6 +19,54 @@ DateObj.prototype.init = function(utils){
 
 
 /**
+ * Checks if a year is aleap year or not
+ * @param {Year} year 
+ * Returns TRUE|FALSE
+ */
+DateObj.prototype.isLeapYear = function(year){
+    return ((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0);
+};
+
+
+/**
+ * Returns the number of days in a month considering the leap years
+ * @param mo   [Integer] Month number (1-12)
+ * @param year [Integer] Year, format: YYYY
+ * @param date [String] Date format YYYY-mm-dd (optional)
+ */
+DateObj.prototype.getdaysinmonth = function(mo, year, date){
+    var monthIndex = mo;
+    var yearIndex = year; 
+
+    if(date !== undefined){
+        if(date.indexOf('-') >= 0){
+            monthIndex = date.split('-')[1];
+            yearIndex = date.split('-')[0];
+        }
+    }
+
+    var months = {
+        1:"31",
+        2:"28",
+        3:"31",
+        4:"30",
+        5:"31",
+        6:"30",
+        7:"31",
+        8:"31",
+        9:"30",
+        10:"31",
+        11:"30",
+        12:"31"
+    };
+
+    // Check if leap year
+    months[2] = this.isLeapYear(yearIndex) ? 29 : 28;
+    return parseInt(months[monthIndex]);
+};
+
+
+/**
  * Get the number of months between (2) dates
  * @param { [String] date to subtract FROM @date2. Format: YYYY/mm/dd or (Year month, date) } date1 
  * @param { [String] date to subtract. Format: YYYY/mm/dd or (Year month, date) } date2 
@@ -80,6 +128,62 @@ DateObj.prototype.getdatefromutc = function(date){
     
     var convert = new Date(date);
     return (convert.getUTCFullYear() + '/' + (convert.getUTCMonth() + 1) + '/' + convert.getUTCDate());
+};
+
+
+/**
+ * Get the numerical start index (day of year, first day) of a month in a year
+ * @param date [String] Date format YYYY-mm-dd
+ * Returns 1-365
+ */
+DateObj.prototype.getdoymonth = function(date){
+    var datesplit = date.split('-');
+
+    var months = {
+        1:"31",
+        2:"28",
+        3:"31",
+        4:"30",
+        5:"31",
+        6:"30",
+        7:"31",
+        8:"31",
+        9:"30",
+        10:"31",
+        11:"30",
+        12:"31"
+    };
+
+    // Check if leap year
+    months[2] = this.isLeapYear(datesplit[0]) ? 29 : 28;
+
+    var sum = 0;
+    date = date.split('-');
+
+    for(mo in months){
+        if(mo < parseInt(date[1])){
+            sum += parseInt(months[mo]);
+      }
+      else{
+            break;
+        }
+    }
+    return sum + 1;
+};
+
+
+/**
+ * Get the numerical index (day of year) of a month in a year
+ * @param date [String] Date format "YYYY-mm-dd" or "YYYY/mm/dd"
+ * Returns 1-365, null if invalid value
+ */
+DateObj.prototype.getdoy = function(date){
+    var curr = new Date(date);
+    var start = new Date(curr.getFullYear(), 0, 0);
+    var difference = (curr - start) + ((start.getTimezoneOffset() - curr.getTimezoneOffset()) * 60 * 1000);
+    var oneDay = 1000 * 60 * 60 * 24;
+    var doy = Math.floor(difference / oneDay);
+    return isNaN(doy) ? null : doy;
 };
 
 
